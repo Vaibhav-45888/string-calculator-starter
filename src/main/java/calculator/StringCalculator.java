d8686601c8838d3cf369615b05db80f14d446915
Vaibhav Srivastava <vaibhavji45888@gmail.com>
2021-10-17 02:00:51 +0530

    First_Condition_Passed

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,6 +2,14 @@ package calculator;
 
 class StringCalculator {
 
-
+	public static int add(String numbers) {
+		int sum = 0;
+		String[] arr = numbers.split(",");
+		for (String number : arr) {
+			if (!number.trim().isEmpty()) {
+				sum = sum + Integer.parseInt(number);
+			}
+		}
+		return sum;
+	}
 }
-
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -5,4 +5,19 @@ import org.junit.jupiter.api.Test;
 
 class StringCalculatorShould {
 
+	@Test
+	public final void whenEmptyStringIsUsed() {
+		Assertions.assertEquals(0, StringCalculator.add(""));
+	}
+
+	@Test
+	public final void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
+		Assertions.assertEquals(1, StringCalculator.add("1"));
+	}
+
+	@Test
+	public final void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
+		Assertions.assertEquals(1 + 2, StringCalculator.add("1,2"));
+	}
+
 }
